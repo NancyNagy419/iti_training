fda3cc90f1fc485ee0a1f0d57572ad6de74ae6e4
nancy <nancynagy419@gmail.com>
2023-07-20 00:22:18 +0300

    add print HelloGit

--- HelloITI.java
+++ HelloITI.java
@@ -1,8 +1,13 @@
 class HelloIti
 {
+	public static String print(){
+		return ""HelloITI"";
+	}
+
 	public static void main(String[] args)
  	{
-		System.out.println("HelloITI");
+		System.out.println(print());
+		System.out.println("HelliGit");
 		
 	}
 
